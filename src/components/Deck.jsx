7d674e7e8b4fd5466507de78ae0eee70d2685759
Anmoldeep <anmolcheema009@gmail.com>
2025-03-04 01:25:54 +0530

    bug-fix: display suit and value on the drawn cards, stored data objects instead of Card component in deckOfCards

--- src/components/Deck.jsx
+++ src/components/Deck.jsx
@@ -7,9 +7,7 @@ const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"
 const deckOfCards = [];
 for (let suit of suits) {
     for (let value of values) {
-        deckOfCards.push(
-        <Card value={value} suit={suit} />
-      );
+        deckOfCards.push({suit, value});
     }
 }
 
