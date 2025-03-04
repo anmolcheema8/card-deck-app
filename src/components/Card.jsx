84fdab57f2caf25758f2a80a16cd3ef08750a3c3
Anmoldeep <anmolcheema009@gmail.com>
2025-03-04 23:35:05 +0530

    update css: display red color for hearts and diamonds

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Card = ({ value, suit, isPicked, onClick }) => {
   return (
     <div 
-      className = {`card ${isPicked ? "picked" : ""}`}
+      className = {`card ${isPicked ? "picked" : ""} ${suit === '♥' || suit === '♦' ? "red-suit" : "black-suit"}`}
       onClick={onClick}
     >
       <p>{value}</p>
