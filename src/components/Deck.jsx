5ddec4442d0e02cdf460f7e84695a64bcfd4993c
Anmoldeep <anmolcheema009@gmail.com>
2025-03-05 07:44:45 +0530

    Add statement of authorship

--- src/components/Deck.jsx
+++ src/components/Deck.jsx
@@ -1,3 +1,6 @@
+/*
+StAuth10244: I Anmoldeep Singh, 000913075 certify that this material is my original work. No other person's work has been used without due acknowledgement. I have not made my work available to anyone else. -->
+*/
 import React, { useState } from "react";
 import Card from "./Card";
 
